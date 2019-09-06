55a0177003cb48691e787ab8b56013db44462db4
admin <dnog91>
2019-09-06 15:43:16 +0100

    DAO Entitys created

---
+++ src/main/java/org/springframework/samples/petclinic/owner/PetDAO.java
@@ -0,0 +1,43 @@
+package org.springframework.samples.petclinic.owner;
+
+import java.util.List;
+
+public class PetDAO implements IPetDAO{
+
+	@Override
+	public Bill findOne(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bill> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(Bill entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Bill update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Bill entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer entityId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
